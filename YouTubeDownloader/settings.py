124f7c329fc984ea302602d02da982f2f0053e3b
marcEssam48 <marcessam48_625@fci.helwan.edu.eg>
2021-06-09 12:28:52 +0200

    Logging added

--- YouTubeDownloader/settings.py
+++ YouTubeDownloader/settings.py
@@ -89,6 +89,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'root',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT' : '5432',
 
     }
 }
