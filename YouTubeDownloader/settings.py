1377677852781ce4f80d1351ac46d7e980e81529
marcEssam48 <marcessam48_625@fci.helwan.edu.eg>
2021-06-09 12:53:29 +0200

    Logging added

--- YouTubeDownloader/settings.py
+++ YouTubeDownloader/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -147,6 +148,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 if os.getcwd() == '/app':
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
+    # Honor the 'X-forwarded-Proto' header for request.is_secure().
     #Honor the 'X-forwarded-Proto' header for request.is_secure().
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
